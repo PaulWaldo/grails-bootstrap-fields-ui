0c60c7540712d1fa9e9c8dffb2bb7bdae23dbc04
Simon Pither <simon@pither.com>
2014-04-18 17:00:47 +0100

    Version 1.0

--- BootstrapFieldsUiGrailsPlugin.groovy
+++ BootstrapFieldsUiGrailsPlugin.groovy
@@ -1,6 +1,6 @@
 class BootstrapFieldsUiGrailsPlugin {
     // the plugin version
-    def version = "1.0.RC4"
+    def version = "1.0"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "2.0 > *"
     // the other plugins this plugin depends on
